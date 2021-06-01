08dba4a9e6a8c477020c8bf6654b06dac6131b12
oysa88 <oystein.salomonsen@tekniskmuseum.no>
2021-06-01 17:31:21 +0200

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onButtonPressed(Button.AB, function () {
+    if (Arm == 0) {
+        Arm = 1
+    } else {
+        Arm = 0
+    }
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+let Roll = 0
+let Pitch = 0
+let Arm = 0
+radio.setGroup(1)
+basic.forever(function () {
+    Pitch = input.rotation(Rotation.Pitch)
+    Roll = input.rotation(Rotation.Roll)
+    radio.sendValue("P", Pitch)
+    radio.sendValue("A", Arm)
+    radio.sendValue("R", Roll)
+})
