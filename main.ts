c4ea43319519838f81ccceec7578ccd2d36d1308
oysa88 <oystein.salomonsen@tekniskmuseum.no>
2021-09-02 13:28:35 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,22 @@
 input.onButtonPressed(Button.AB, function () {
-    if (Arm == 0) {
-        Arm = 1
+    if (AvPå == 0) {
+        AvPå = 1
     } else {
-        Arm = 0
+        AvPå = 0
     }
 })
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
-    Arm = 0
+    AvPå = 0
 })
 let Roll = 0
 let Pitch = 0
-let Arm = 0
+let AvPå = 0
 radio.setGroup(1)
 basic.forever(function () {
     Pitch = input.rotation(Rotation.Pitch)
     Roll = input.rotation(Rotation.Roll)
     radio.sendValue("P", Pitch)
-    radio.sendValue("A", Arm)
+    radio.sendValue("A", AvPå)
     radio.sendValue("R", Roll)
 })
